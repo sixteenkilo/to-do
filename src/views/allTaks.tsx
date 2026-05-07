93da06f00d256e3eb01ef57a73063da8b04053b4
mrTebrom <mr.tebron@gmail.com>
2026-05-07 10:47:13 +0500

    fix: prevent infinite render loop in AllTasksView

--- src/views/allTaks.tsx
+++ src/views/allTaks.tsx
@@ -5,12 +5,14 @@ import { TaskElement } from "../components/taskElement";
 const { Title } = Typography;
 
 export const AllTaksView = () => {
-  const tasksData = useTask((s) => [...s.tasks]).sort((a, b) => {
+  const tasks = useTask((s) => s.tasks);
+
+  const tasksData = [...tasks].sort((a, b) => {
     if (!a.dueDate) return 1;
     if (!b.dueDate) return -1;
+
     return new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime();
   });
-
   return (
     <Flex vertical style={{ height: "100%" }}>
       <Flex flex={0} style={{ width: "100%" }}>
